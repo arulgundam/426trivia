d2217f1e43c2df7a1811f57ef8c6bb29ad27d0be
Arun Somasundaram <arunrsoma@gmail.com>
2024-12-02 22:04:59 -0500

    Profile.css updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,26 @@
+import "./Profile.css";
+
 const Profile = ({ username, userData, goToHome, logout, deleteAccount}) => {
   if (!userData) return <p>Loading profile...</p>;
 
   return (
-    <div>
-      <h1>{username}'s Profile</h1>
-      <p>Total Points: {userData.totalPoints}</p>
+    <div className="profile">
+      <h1 className="my_profile">{username}'s Profile</h1>
+      <p className="total">Total Points: {userData.totalPoints}</p>
       <h3>Category Points:</h3>
       {Object.entries(userData.categoryPoints).map(([category, points]) => (
         <p key={category}>
           {category}: {points}
         </p>
       ))}
-      <button onClick={goToHome}>Go Home</button>
-      <button onClick={logout}>Logout</button>
-      <button onClick={deleteAccount}>DeleteAccount</button>
+      <div>
+      <button className="button" id="home"onClick={goToHome}>Go Home</button>
+      <button className="button" id="logout"onClick={logout}>Logout</button>
+      <button className="button" id="delete"onClick={deleteAccount}>DeleteAccount</button>
+      </div>
     </div>
   );
 };
 
 export default Profile;
+
